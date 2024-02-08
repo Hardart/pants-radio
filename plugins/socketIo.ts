637a67a49770716b2658a93d8fbcd87f2ab31a67
Hardart <kaino@bk.ru>
2024-02-08 19:25:14 +0300

    change socket url

--- plugins/socketIo.ts
+++ plugins/socketIo.ts
@@ -3,13 +3,13 @@ import io from 'socket.io-client'
 export default defineNuxtPlugin(() => {
   if (!process.client) return
 
-  const url = `http://localhost:`
+  const url = `http://localhost:3068`
 
   if (!url || typeof url !== 'string') return
 
   return {
     provide: {
-      ws: (port: number, token: string | null) => io(),
+      ws: (port: number, token: string | null) => io(process.dev ? url : ''),
     },
   }
 })

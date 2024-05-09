74a7320d0b3e3da6e39c64fc748eb011b600a86b
Hardart <kaino@bk.ru>
2024-05-10 01:58:04 +0300

    remove spaces in config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,29 +2,32 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   colorMode: {
-    preference: 'light',
+    preference: 'light'
   },
   app: {
     head: {
       script: [{ src: '/js/uikit.min.js', defer: true }],
       title: '«Радио ШТАНЫ». Все только для тебя',
       meta: [
-        { name: 'description', content: 'Слушай Радио ШТАНЫ в прямом эфире онлайн на официальном сайте бесплатно и без регистрации' },
+        {
+          name: 'description',
+          content: 'Слушай Радио ШТАНЫ в прямом эфире онлайн на официальном сайте бесплатно и без регистрации'
+        },
         {
           name: 'keywords',
           content:
-            'радио, онлайн-радио, интернет-радио, бесплатное радио, радиостанция, музыка, новости, слушать радио, слушать радио онлайн, слушать online радио, Радио, слушать онлайн',
-        },
-      ],
-    },
+            'радио, онлайн-радио, интернет-радио, бесплатное радио, радиостанция, музыка, новости, слушать радио, слушать радио онлайн, слушать online радио, Радио, слушать онлайн'
+        }
+      ]
+    }
   },
   modules: ['@pinia/nuxt', '@nuxt/ui', '@vueuse/nuxt', 'nuxt-icon', '@nuxtjs/device', '@morev/vue-transitions/nuxt'],
   pinia: {
-    storesDirs: ['./store/**'],
+    storesDirs: ['./store/**']
   },
   routeRules: {
     '/api/v1/**': {
-      proxy: process.env.BASE_URL,
-    },
-  },
+      proxy: process.env.BASE_URL
+    }
+  }
 })

8f8f4a5f580a21a51fa4b26f16f842ab5cfabdc7
Hardart <kaino@bk.ru>
2024-06-05 00:15:50 +0300

    fix logo animation

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
 defineProps(['size', 'isMain'])
-const { y } = useWindowScroll()
 const { isMobile } = useDevice()
+const { y } = useWindowScroll()
 const scroll = ref(130)
-const maxValue = 130
-const minValue = 60
-const startFrom = ref(maxValue)
 const translateY = ref(35)
-const delta = 4
-
-const { pause, resume } = useRafFn(
-  () => {
-    const initValue = y.value < minValue ? maxValue - y.value : clamp(y.value, maxValue, minValue)
-    scroll.value = clamp((startFrom.value -= delta), initValue, maxValue)
-    translateY.value = (35 * (scroll.value - minValue)) / (maxValue - minValue)
-    if (scroll.value == minValue || scroll.value == maxValue - y.value) pause()
-  },
-  { immediate: false }
-)
 onMounted(() => {
-  resume()
-})
+  const maxValue = 130
+  const minValue = 60
+  const startFrom = ref(maxValue)
+  const delta = 4
 
-watch(y, () => {
-  scroll.value = clamp(130 - y.value, 60, 130)
-  translateY.value = (35 * (scroll.value - 60)) / 70
+  const { pause } = useRafFn(
+    () => {
+      const initValue = y.value < minValue ? maxValue - y.value : clamp(y.value, maxValue, minValue)
+      scroll.value = clamp((startFrom.value -= delta), initValue, maxValue)
+      translateY.value = (35 * (scroll.value - minValue)) / (maxValue - minValue)
+      if (scroll.value == minValue || scroll.value == maxValue - y.value) pause()
+    },
+    { immediate: true }
+  )
+
+  watch(y, () => {
+    scroll.value = clamp(130 - y.value, 60, 130)
+    translateY.value = (35 * (scroll.value - 60)) / 70
+  })
 })
 </script>
 
@@ -37,17 +36,7 @@ watch(y, () => {
   <div v-else class="relative flex items-center" :style="!isMobile && `width: ${scroll}px;`">
     <NuxtLink to="/">
       <img v-if="isMobile" src="/logo.svg" width="60" height="60" class="ml-2" alt="" />
-      <img
-        v-else
-        src="/logo.svg"
-        :width="scroll"
-        :height="scroll"
-        class="ml-2"
-        :style="`width: ${scroll}px; transform: translateY(${translateY}px);`"
-        alt=""
-      />
+      <img v-else src="/logo.svg" class="ml-2 w-full" :style="`transform: translateY(${translateY}px)`" alt="" />
     </NuxtLink>
   </div>
 </template>
-
-<style></style>

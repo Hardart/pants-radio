9954109f5c55d8d03aaaab2a8fbd1575ebae72f9
Hardart <kaino@bk.ru>
2024-07-29 23:53:17 +0300

    change icons view on button

--- components/sections/playlist/PlaylistTrackList.vue
+++ components/sections/playlist/PlaylistTrackList.vue
@@ -1,14 +1,13 @@
-<script setup lang='ts'>
-import type { Track } from '~/types/track';
+<script setup lang="ts">
+import type { Track } from '~/types/track'
 
 defineProps<{
   tracks: Track[]
 }>()
 </script>
- 
+
 <template>
   <Section v-if="tracks" padding="small">
     <TrackList :tracks :show-date="true" />
   </Section>
 </template>
- 

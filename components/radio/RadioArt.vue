30ad3c11b4a3a44692a222dd5697a12f7788855b
Hardart <kaino@bk.ru>
2024-08-04 23:17:15 +0300

    add lazy loading to image in radio art

--- components/radio/RadioArt.vue
+++ components/radio/RadioArt.vue
@@ -6,6 +6,6 @@ defineProps<{
 
 <template>
   <div class="size-[60px]">
-    <img :src class="size-full rounded-md" alt="" width="60" height="60" />
+    <img :src class="size-full rounded-md" alt="" width="60" height="60" loading="lazy" />
   </div>
 </template>

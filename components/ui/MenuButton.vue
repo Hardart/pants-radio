070f767c5de53e8254ccadcad22d6f064b753241
Hardart <kaino@bk.ru>
2024-08-31 22:43:38 +0300

    style: change responsive styles of MenuButton

--- components/ui/MenuButton.vue
+++ components/ui/MenuButton.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-2 lg:p-5 text-neutral-100 hover:bg-slate-400/20 cursor-pointer ">
-    <Icon name="heroicons:bars-3-bottom-left-20-solid" size="40"/>
+  <div class="z-10 cursor-pointer p-2 text-neutral-100 hover:bg-slate-400/20 max-md:ml-auto lg:p-5">
+    <Icon name="heroicons:bars-3-bottom-left-20-solid" size="40" />
   </div>
 </template>
 

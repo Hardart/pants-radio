8e82a9483ccf2f4a40effd5fec0c2904885290e7
Hardart <kaino@bk.ru>
2024-08-31 22:42:43 +0300

    style: change responsive styles of TrackList, MenuButton and Logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 defineProps(['size', 'isMain'])
-const { isMobile } = useDevice()
 const { y } = useWindowScroll()
+
 const scroll = ref(130)
 const translateY = ref(35)
+
 onMounted(() => {
   const maxValue = 130
   const minValue = 60
   const startFrom = ref(maxValue)
   const delta = 4
-
   const { pause } = useRafFn(
     () => {
       const initValue = y.value < minValue ? maxValue - y.value : clamp(y.value, maxValue, minValue)
@@ -33,10 +33,12 @@ onMounted(() => {
       <img src="/logo.svg" :width="size" :height="size" alt="logo" />
     </NuxtLink>
   </div>
-  <div v-else class="relative flex items-center" :style="!isMobile && `width: ${scroll}px;`">
-    <NuxtLink to="/">
-      <img v-if="isMobile" src="/logo.svg" width="60" height="60" class="ml-2" alt="" />
-      <img v-else src="/logo.svg" class="ml-2 w-full" :style="`transform: translateY(${translateY}px)`" alt="" />
+  <div v-else class="absolute left-0 right-0 flex items-center justify-center md:relative md:ml-2 md:max-w-[130px]">
+    <NuxtLink to="/" class="block md:hidden">
+      <img src="/logo.svg" width="60" height="60" alt="" />
+    </NuxtLink>
+    <NuxtLink to="/" :style="{ width: `${scroll}px` }" class="hidden md:block">
+      <img src="/logo.svg" class="w-full" :style="{ transform: `translateY(${translateY}px)` }" alt="" />
     </NuxtLink>
   </div>
 </template>

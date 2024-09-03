a31bc086452f7e597b84ef3b0d9ba44c8bfe29a1
Hardart <kaino@bk.ru>
2024-09-03 22:29:28 +0300

    refactor: correct playback info and adjust some styles

--- components/sections/playlist/PlaylistMainInfo.vue
+++ components/sections/playlist/PlaylistMainInfo.vue
@@ -1,21 +1,13 @@
-<script setup lang='ts'>
+<script setup lang="ts"></script>
 
-</script>
- 
 <template>
-<Section padding="topSmall">
-  <SectionTitle title="Ранее в эфире">
-    <template #desc>
-      <div class="px-2 mt-2 mb-5 leading-4 text-secondary/45 text-xs -space-y-0.5">
-        <p>Пока доступен только список треков, которые прозвучали в московском (федеральном) эфире.</p>
-        <p>
-          Плейлист в других городах обычно ничем не отличается, а лишь смещается относительно разницы во времени между вашим городом и
-          Москвой.
-        </p>
-      </div>
-    </template>
-  </SectionTitle>
-</Section>
+  <Section padding="topSmall">
+    <SectionTitle title="Ранее в эфире">
+      <template #desc>
+        <SectionsPlaylistPlaybackInfo class="max-md:hidden" />
+      </template>
+    </SectionTitle>
+  </Section>
 </template>
- 
+
 <style></style>

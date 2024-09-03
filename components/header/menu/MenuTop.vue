1d376bbb69dd7771a030331cfd8c72078b0ef2b4
Hardart <kaino@bk.ru>
2024-09-03 20:54:10 +0300

    chore: change mobile logo

--- components/header/menu/MenuTop.vue
+++ components/header/menu/MenuTop.vue
@@ -3,10 +3,10 @@ defineEmits(['toggleMenuState'])
 </script>
 
 <template>
-  <div class="container mx-auto my-2 flex items-center justify-between px-3 text-neutral-100">
-    <img src="/assets/logo_text.svg" class="h-12" />
-    <div class="cursor-pointer p-2 hover:bg-neutral-700/30" @click="$emit('toggleMenuState')">
-      <Icon name="material-symbols:close-rounded" size="50" />
+  <div class="container mx-auto my-2 flex items-center justify-between text-neutral-100 lg:my-4">
+    <img src="/assets/logo-text.svg" class="ml-4 h-8 md:h-10" />
+    <div class="mr-2 cursor-pointer p-2 hover:bg-neutral-700/30" @click="$emit('toggleMenuState')">
+      <Icon name="material-symbols:close-rounded" size="40" />
     </div>
   </div>
 </template>

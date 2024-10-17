21e0303b3b7b015962c19afb9092c8ca08020135
Hardart <kaino@bk.ru>
2024-10-17 18:18:46 +0300

    feat: show text message to user if selected archive date is emptey

--- components/track/TrackList.vue
+++ components/track/TrackList.vue
@@ -9,7 +9,9 @@ const { isTrackPlaying, onPlayPreview, isTrackFetching } = useMediaStore()
 
 <template>
   <div class="grid grid-cols-1 gap-2 md:gap-5 lg:grid-cols-2">
+    <h4 v-if="!tracks.length">Что-то пошло не так и история треков не сохранилась, приносим свои извинения</h4>
     <Track
+      v-else
       v-for="track in tracks"
       :track
       :show-date

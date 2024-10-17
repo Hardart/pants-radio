7825448380ab5d472c7b0d47cfa54a9d49ddee1a
Hardart <kaino@bk.ru>
2024-10-17 18:20:18 +0300

    feat: add new skeleton component for archive tracks

---
+++ components/sections/playlist/PlaylistSkeletonList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Section padding="small">
+    <div class="grid grid-cols-1 gap-2 md:gap-5 lg:grid-cols-2">
+      <UiTrackSkeleton v-for="item in 16" />
+    </div>
+  </Section>
+</template>
---
+++ components/ui/TrackSkeleton.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="relative flex gap-x-1.5 rounded-lg bg-gray-300 px-3 py-2 md:gap-x-3 md:px-6 md:py-4">
+    <div class="size-[50px] animate-pulse rounded-full bg-gray-50 md:size-[60px]"></div>
+    <div class="size-[50px] animate-pulse rounded bg-gray-50 md:size-[60px]"></div>
+    <div class="flex flex-1 animate-pulse flex-col gap-3">
+      <div class="h-4 w-1/2 rounded-md bg-gray-50"></div>
+      <div class="h-3 w-1/3 rounded-md bg-gray-50"></div>
+    </div>
+  </div>
+</template>

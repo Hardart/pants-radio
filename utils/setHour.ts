55c5f48834d0cca739336ed31aa4b5a0439d3829
Hardart <kaino@bk.ru>
2024-10-17 18:09:07 +0300

    chore: rename and add types to helper function

--- utils/setHour.ts
+++ utils/setHour.ts
@@ -1,8 +1,8 @@
-export const hoursForSelect = ['00', '03', '06', '09', '12', '15', '18', '21']
+export const hoursForSelect = ['00', '03', '06', '09', '12', '15', '18', '21'] as const
 
 export const filteredHours = (n: number) => hoursForSelect.filter((item) => +item <= n)
 
-export const setHour = () => {
+export const hoursStartFrom: () => (typeof hoursForSelect)[number] = () => {
   const hh = new Date().getHours()
   return [...hoursForSelect].reverse().find((hour) => +hour <= hh) || '00'
 }

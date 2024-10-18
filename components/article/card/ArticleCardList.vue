cc1c8bc4b71a8af6de52745d972f9af26769f029
Hardart <kaino@bk.ru>
2024-10-18 20:03:00 +0300

    style: adjust vertical gap between news cards

--- components/article/card/ArticleCardList.vue
+++ components/article/card/ArticleCardList.vue
@@ -4,7 +4,7 @@ defineProps<{ articles: Article[] }>()
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-6 px-0 md:grid-cols-2 lg:grid-cols-3 lg:gap-4 xl:grid-cols-4">
+  <div class="grid grid-cols-1 gap-6 px-0 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-6 xl:grid-cols-4">
     <ArticleCard v-for="article in articles" :article />
   </div>
 </template>

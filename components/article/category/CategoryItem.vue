33098826da59098edfa81b46ddf027b8a518321e
Hardart <kaino@bk.ru>
2024-10-18 20:01:56 +0300

    refactor: create separate component for category list items at news page

---
+++ components/article/category/CategoryItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { Category } from '~/types/article'
+
+defineProps<{
+  category: Category
+}>()
+</script>
+
+<template>
+  <li class="whitespace-nowrap rounded-md border border-primary px-3 max-md:text-xs md:rounded-xl md:py-1">
+    <NuxtLink :to="`/news/${category.slug}`">{{ category.title }}</NuxtLink>
+  </li>
+</template>
---
+++ components/article/category/CategoryList.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { Category } from '~/types/article'
+
+defineProps<{
+  categories: Category[]
+}>()
+</script>
+
+<template>
+  <ul class="mb-6 flex flex-wrap gap-2">
+    <ArticleCategoryItem v-for="category in categories" :category />
+  </ul>
+</template>

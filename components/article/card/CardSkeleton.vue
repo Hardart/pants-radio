5a9983463aa581562cb95493164e963209d2d498
Hardart <kaino@bk.ru>
2024-10-18 20:04:49 +0300

    feat: create new skeleton component for loading news

---
+++ components/article/card/CardSkeleton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <article class="animation-pulse group relative flex w-full flex-col overflow-hidden rounded-xl bg-white shadow-md">
+    <header class="aspect-video w-full bg-gray-300"></header>
+
+    <div class="animate-pulse space-y-4 bg-gray-100 px-4 py-4 md:space-y-6 lg:space-y-4">
+      <div class="h-3 w-full rounded bg-gray-300"></div>
+      <div class="h-3 w-4/5 rounded bg-gray-300"></div>
+      <div class="h-2 w-1/4 rounded bg-gray-300"></div>
+    </div>
+  </article>
+</template>
---
+++ components/article/card/SkeletonCardList.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid grid-cols-1 gap-6 px-0 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-6 xl:grid-cols-4">
+    <ArticleCardSkeleton v-for="item in 8" />
+  </div>
+</template>

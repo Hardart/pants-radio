04090885a16dfab27a482e6d2aa4a7f5678bf0d7
Hardart <kaino@bk.ru>
2024-12-12 22:53:43 +0300

    feat: add new helper for change slide size

--- utils/correctImageSrc.ts
+++ utils/correctImageSrc.ts
@@ -1,3 +1,9 @@
 export function correctImageSrc(src: string | undefined) {
   return process.dev ? 'http://hardart.site' + src : src
 }
+
+export function setSlideshowSrcset(src: string | undefined, size: string) {
+  if (!src) return ''
+  src = src.replace('1536x658', size)
+  return correctImageSrc(src)
+}

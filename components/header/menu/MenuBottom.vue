b1753aa0015101fcb70832f850b963d13759bd70
Hardart <kaino@bk.ru>
2024-12-12 18:40:52 +0300

    feat: add new /privacy and /information pages

--- components/header/menu/MenuBottom.vue
+++ components/header/menu/MenuBottom.vue
@@ -1,12 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits(['toggleMenuState'])
+</script>
 
 <template>
   <div class="container mx-auto">
-    <div class="flex items-center justify-center md:justify-between text-neutral-500 mb-8 max-lg:mt-4">
+    <div class="mb-8 flex items-center justify-center text-neutral-500 max-lg:mt-4 md:justify-between">
       <SocialIcons hover-color="primary" hide-on-small-screen />
-      <div class="flex max-md:flex-col items-center text-sm">
-        <NuxtLink class="underline md:pr-3 md:mr-3 md:border-r border-neutral-500 hover:text-neutral-400">Правовая информация</NuxtLink>
-        <NuxtLink class="underline hover:text-neutral-400">Политика конфиденциальности</NuxtLink>
+      <div class="flex items-center text-sm max-md:flex-col">
+        <NuxtLink
+          to="/information"
+          class="border-neutral-500 underline hover:text-neutral-400 md:mr-3 md:border-r md:pr-3"
+          exact-active-class="text-neutral-400"
+          @click="$emit('toggleMenuState')"
+        >
+          Правовая информация
+        </NuxtLink>
+        <NuxtLink
+          to="/privacy"
+          class="underline hover:text-neutral-400"
+          exact-active-class="text-neutral-400"
+          @click="$emit('toggleMenuState')"
+        >
+          Политика конфиденциальности
+        </NuxtLink>
       </div>
     </div>
   </div>

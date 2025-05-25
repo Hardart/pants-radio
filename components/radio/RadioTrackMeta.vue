9e7f5e441576749c61d3c34a1f4e91a4fbdff7d6
Hardart <kaino@bk.ru>
2025-05-25 16:26:35 +0300

    style: remove class from RadioTrackMeta component

--- components/radio/RadioTrackMeta.vue
+++ components/radio/RadioTrackMeta.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="scrollbar-hide flex-grow overflow-hidden overflow-x-auto whitespace-nowrap text-neutral-100">
+  <div class="scrollbar-hide flex-grow overflow-hidden overflow-x-auto text-neutral-100">
     <h4 class="line-clamp-1 md:text-lg md:font-semibold">{{ artistName }}</h4>
     <p class="line-clamp-1 text-xs font-thin md:text-sm">{{ trackTitle }}</p>
   </div>

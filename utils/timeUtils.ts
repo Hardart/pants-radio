e92ad8b3436829219a5bed97dcda1acdcac10773
Hardart <kaino@bk.ru>
2025-07-09 01:11:30 +0300

    feat: add new util - time util

---
+++ utils/timeUtils.ts
@@ -0,0 +1,12 @@
+// Конвертация минут в "HH:MM"
+export function minutesToTime(minutes: number): string {
+  const hour = Math.floor(minutes / 60)
+  const minute = minutes % 60
+  return `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`
+}
+
+// Конвертация времени из "HH:MM" в минуты
+export function timeToMinutes(time: string): number {
+  const [h, m] = time.split(':').map(Number)
+  return h * 60 + m
+}

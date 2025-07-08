ec7c5a49530734b6ef559ab2de6f2e6f03551187
Hardart <kaino@bk.ru>
2025-07-09 01:07:56 +0300

    chore: change data attribute position in element string

--- components/programs/ProgramsList.vue
+++ components/programs/ProgramsList.vue
@@ -7,7 +7,7 @@ defineProps<{ programs: Program[] }>()
 <template>
   <div class="grid max-w-4xl gap-4 sm:grid-cols-2 md:grid-cols-3" container>
     <div v-for="program in programs" class="relative flex flex-col rounded-3xl border bg-white p-2 shadow-lg" card>
-      <div header class="aspect-square overflow-hidden">
+      <div class="aspect-square overflow-hidden" header>
         <img v-if="program.image" :src="correctImageSrc(program.image)" class="size-full rounded-2xl object-cover" alt="" />
         <div v-else class="size-full rounded-2xl bg-zinc-300 object-cover"></div>
       </div>

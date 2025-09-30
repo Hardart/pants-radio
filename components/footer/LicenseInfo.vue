2e5ee3e669a49f07156822f97ef12e012e5a63ab
Hardart <kaino@bk.ru>
2025-09-30 21:51:50 +0300

    style: change vertical spacing in footer license info

--- components/footer/LicenseInfo.vue
+++ components/footer/LicenseInfo.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <div class="flex items-center px-2 pb-8 max-sm:flex-col sm:space-x-8">
     <UiRateIcon class="max-sm:hidden" :value="info.iconValue" />
-    <div class="space-y-4 text-sm leading-4 max-sm:text-center md:text-base" v-html="info.text" />
+    <div class="space-y-1 text-sm leading-4 max-sm:text-center md:text-base" v-html="info.text" />
   </div>
 </template>
 

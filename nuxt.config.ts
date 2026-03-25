029cb862e0bcea7703ef9069f120320821a426dc
Hardart <kaino@bk.ru>
2026-03-24 22:45:17 -0700

    fix: return to original radio URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      radioURL: 'http://23.105.253.236:8000/rsh_federal'
+      radioURL: 'https://stream.elarin.ru/rsh_federal'
     }
   },
   modules: ['@pinia/nuxt', '@nuxt/ui', '@vueuse/nuxt', 'nuxt-icon', '@nuxtjs/device', '@morev/vue-transitions/nuxt'],
